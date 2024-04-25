eae502dc924fa1ce9dc472b820dd54b3ea8894e8
sb2bg <sullivanbognar@gmail.com>
2024-04-25 18:21:58 -0500

    Remove database

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,17 @@ import 'package:flow/pages/home_page.dart';
 import 'package:flow/util/action_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:ollama_dart/ollama_dart.dart';
 
 Future<void> main() async {
   const config = MacosWindowUtilsConfig();
   await config.apply();
-  await initDatabase();
 
   runApp(const MyApp());
 }
 
-late final Database database;
-final chatActionNotifier = ChatActionNotifier();
-
-Future<void> initDatabase() async {
-  database = await openDatabase(
-    'flow.db',
-    version: 1,
-    onCreate: (db, version) async {
-      await db.execute('''
-        CREATE TABLE IF NOT EXISTS conversations (
-          model TEXT NOT NULL UNIQUE PRIMARY KEY,
-          name TEXT NOT NULL,
-          version TEXT NOT NULL,
-          created_at TEXT NOT NULL,
-          updated_at TEXT NOT NULL
-        )
-      ''');
-
-      await db.execute('''
-        CREATE TABLE IF NOT EXISTS messages (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          conversation_id INTEGER NOT NULL,
-          message TEXT NOT NULL,
-          created_at TEXT NOT NULL,
-          updated_at TEXT NOT NULL,
-          FOREIGN KEY (conversation_id) REFERENCES conversations (id)
-        )
-      ''');
-    },
-  );
-}
+final chatActionNotifier = ActionNotifier();
+final client = OllamaClient();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
