8d330ed0ef818952f5d6ad0d177746faf4d04058
sb2bg <sullivanbognar@gmail.com>
2024-05-01 13:16:03 -0500

    Refactor LLMInterfaceContentState to improve message sending and regeneration

--- lib/pages/llm_page.dart
+++ lib/pages/llm_page.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:file_picker/file_picker.dart';
 import 'package:flow/main.dart';
 import 'package:flow/util/constants.dart';
@@ -80,39 +79,17 @@ class _LLMInterfaceContentState extends State<LLMInterfaceContent> {
     }
   }
 
-  Future<void> _sendMessage() async {
-    if (_waiting) {
-      return;
-    }
-
-    final message = _controller.text.trim();
-
-    if (message.isEmpty && _pendingImages.isEmpty) {
-      return;
-    }
-
-    _controller.clear();
-
-    final images = _pendingImages.keys.toList();
-    final previews = _pendingImages.values.toList();
-    _pendingImages.clear();
-
-    setState(() {
-      final userMessage = images.isEmpty
-          ? Message(role: MessageRole.user, content: message)
-          : Message(role: MessageRole.user, content: message, images: images);
-
-      _chatHistory.add(userMessage);
-      _sentImages.add(previews);
-      _waiting = true;
-    });
-
+  Future<void> _queryWith(List<Message> messages) async {
     final name = widget.model.name;
 
     if (name == null) {
       context.showNonFatalError('Model name is null');
       return;
     }
+
+    setState(() {
+      _waiting = true;
+    });
 
     final stream = client.generateChatCompletionStream(
       request: GenerateChatCompletionRequest(
@@ -147,6 +124,46 @@ class _LLMInterfaceContentState extends State<LLMInterfaceContent> {
     });
   }
 
+  Future<void> _sendMessage() async {
+    if (_waiting) {
+      return;
+    }
+
+    final message = _controller.text.trim();
+
+    if (message.isEmpty && _pendingImages.isEmpty) {
+      return;
+    }
+
+    _controller.clear();
+
+    final images = _pendingImages.keys.toList();
+    final previews = _pendingImages.values.toList();
+    _pendingImages.clear();
+
+    setState(() {
+      final userMessage = images.isEmpty
+          ? Message(role: MessageRole.user, content: message)
+          : Message(role: MessageRole.user, content: message, images: images);
+
+      _chatHistory.add(userMessage);
+      _sentImages.add(previews);
+    });
+
+    await _queryWith(_chatHistory);
+  }
+
+  Future<void> regenerateFrom(int index) async {
+    // remove all messages, images, and stops after the index
+    setState(() {
+      _chatHistory.removeRange(index, _chatHistory.length);
+      // have to divide image index by 2 because we only store images for the user which is every other message
+      _sentImages.removeRange((index ~/ 2) + 1, _sentImages.length);
+    });
+
+    await _queryWith(_chatHistory);
+  }
+
   bool get canSubmit =>
       _controller.text.trim().isNotEmpty || _pendingImages.isNotEmpty;
 
@@ -155,7 +172,11 @@ class _LLMInterfaceContentState extends State<LLMInterfaceContent> {
     return Column(
       children: [
         Expanded(
-            child: ChatHistory(messages: _chatHistory, images: _sentImages)),
+            child: ChatHistory(
+                messages: _chatHistory,
+                images: _sentImages,
+                onRegenerateAnswer: regenerateFrom,
+                waiting: _waiting)),
         Padding(
           padding: const EdgeInsets.all(4.0),
           child: KeyboardListener(
